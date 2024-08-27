ebaaca044ed2f6716429082b0e7debc67e6e0e12
IliassN <2277205@cegepmontpetit.ca>
2024-08-27 08:45:51 -0400

    Exercices Triangles.kt
    Complétion du code pour l'exercice Triangles.kt

--- code/testFirstProject/src/main/kotlin/Triangles.kt
+++ code/testFirstProject/src/main/kotlin/Triangles.kt
@@ -1,7 +1,20 @@
 package org.nafile
 
 fun triangle(hauteur : Int) : String {
+    val sb = StringBuilder();
+    for (i in 1..hauteur)
+    {
+        for(o in 1.. i)
+        {
+            sb.append("*")
+        }
+        sb.append("\n")
+    }
 
+    return sb.toString()
+}
 
-    return "TODO"
+fun main()
+{
+    print(triangle(5))
 }
