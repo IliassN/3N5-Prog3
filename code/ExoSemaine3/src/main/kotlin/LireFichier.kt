c1234b142eb260a5ddceb947b48a69c6747d5b76
Ilias <2277205@cegepmontpetit.ca>
2024-09-10 10:02:14 -0400

    Exo LireFichier
    Complétion du code de l'exo LireFichier

---
+++ code/ExoSemaine3/src/main/kotlin/LireFichier.kt
@@ -0,0 +1,26 @@
+import java.io.File
+
+fun main(args: Array<String>){
+    var nomFichier = args[0]
+    var nomFichier2 = args[1]
+    var fichier = File(nomFichier)
+    var fichier2 = File(nomFichier2)
+    if(fichier.exists())
+    {
+        var texte1 = fichier.readText()
+        println(texte1)
+
+    }
+    else{
+        println("Le fichier $nomFichier n'existe pas!")
+    }
+    if(fichier2.exists())
+    {
+        var texte2 = fichier2.readText()
+        println(texte2)
+    }
+    else
+    {
+        println("Le fichier $nomFichier2 n'existe pas!")
+    }
+}
